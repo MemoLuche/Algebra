0d69d910a99611c057df31266b0492d9e16caa42
MemoLuche <guillermogp08@gmail.com>
2024-04-05 15:44:17 -0600

    Se modifica el menu, y se arreglan bugs de Codificar.py

--- src/Criptografia/Codificar.py
+++ src/Criptografia/Codificar.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 # Generar matriz codigo aleatorio
-def generar_matriz_enteros(n):
+def generarMatrizCodigo(n):
     # Generar una matriz aleatoria con números enteros entre -10 y 10
     matriz = np.random.randint(-10, 11, size=(n, n))
     
@@ -12,9 +12,9 @@ def generar_matriz_enteros(n):
         if np.all(inversa.astype(int) == inversa):  # Verificar si todos los elementos de la inversa son enteros
             return matriz
         else:
-            return generar_matriz_enteros(n)  # Si la inversa no tiene números enteros, generar otra matriz
+            return generarMatrizCodigo(n)  # Si la inversa no tiene números enteros, generar otra matriz
     except np.linalg.LinAlgError:  # Si la matriz no es invertible, generar otra matriz
-        return generar_matriz_enteros(n)
+        return generarMatrizCodigo(n)
 
 dicc = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10,
                     'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19,
@@ -23,62 +23,70 @@ dicc = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 
 dicc_inverso = {valor: clave for clave, valor in dicc.items()}
 
 # Ingreso del mensaje y transformación
-def ingresarFrase():
+def ingresarMsg():
+    print('Ingresar la frase a encriptar')
     frase = input()
     fraseEnNum = list(dicc[letra] for letra in frase.upper())
     return fraseEnNum
 
 # Convertir frase en una matriz
-def matrizMensaje(m1):
-    n = math.ceil(math.sqrt(len(m1)))
+def generarMatrizMsg(mensaje):
+    n = math.ceil(math.sqrt(len(mensaje)))
     # Calcular la cantidad de elementos que faltan para que la matriz sea cuadrada
-    faltantes = n**2 - len(m1)
+    faltantes = n**2 - len(mensaje)
 
     # Agregar espacios adicionales si es necesario
-    m1.extend([27] * faltantes)
+    mensaje.extend([27] * faltantes)
 
-    mensaje = np.array(m1).reshape(n, n)
+    matriz = np.array(mensaje).reshape(n, n)
     print('Matriz mensaje:')
-    print(mensaje)
-    return mensaje
+    print(matriz)
+    return matriz
 
 # Conseguir el valor de nXn
-def conseguirN(m1):
-    n = math.ceil(math.sqrt(len(m1)))
+def conseguirN(mensaje):
+    n = math.ceil(math.sqrt(len(mensaje)))
     return n
 
 # Codificar el mensaje
-def codificar(mensaje, mCodigo):
-    mCodificado = np.dot(mensaje, mCodigo)
+def codificar(mMensaje, mCodigo):
+    mCodificado = np.dot(mMensaje, mCodigo)
     print('Matriz codificada')
     print(mCodificado)
     return mCodificado
 
 # Generar la matriz para descifrar
-def matrizDescifrar(mCodigo):
+def invertirMatriz(mCodigo):
     mCodigoDescifrar = np.linalg.inv(mCodigo)
     print('Matriz descifrado')
     print(mCodigoDescifrar)
     return mCodigoDescifrar
 
 # Insertar la matriz para descifrar
-def ingresarMatrizCodigoInverso():
-    m1 = list(input().split(' '))
-    n = math.ceil(math.sqrt(len(m1)))
-    codigoInverso = np.array(m1).reshape(n, n)
+def ingresarCodigoInverso():
+    print('Ingresa la matriz inversa separando los valores con espacios')
+    mCodigo = list(map(int, input().split(' ')))
+    n = math.ceil(math.sqrt(len(mCodigo)))
+    codigoInverso = np.array(mCodigo).reshape(n, n)
     return codigoInverso
 
 # Descifrar el mensaje
-def descifrarMensaje(mCDescifrar, mCodificado):
-    mDescifrado = np.dot(mCodificado, mCDescifrar)
+def descifrarMensaje(mCodificado, cInverso):
+    mDescifrado = np.dot(mCodificado, cInverso)
     print('Matriz decodificada')
     print(mDescifrado)
     return mDescifrado
 
 # Convertir los números descifrados a letras
 def convertirMensajeDescifrado(mDescifrado):
-    fraseTransformada = [dicc_inverso[int(letra)] for fila in mDescifrado for letra in fila]
+    fraseTransformada = [dicc_inverso[round(letra)] for fila in mDescifrado for letra in fila]
     cadena = ''.join(fraseTransformada)
     print('Mensaje descodificado:')
     print(cadena)
     return cadena
+
+def ingresarMensajeEncriptado(n1, n2):
+    print('Ingresa el mensaje a decodificar')
+    m1 = list(map(int, input().split(' ')))
+    msgEncriptado = np.array(m1).reshape(n1, n2)
+    return msgEncriptado
--- src/Criptografia/main.py
+++ src/Criptografia/main.py
@@ -6,29 +6,34 @@ dicc = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 
 
 dicc_inverso = {valor: clave for clave, valor in dicc.items()}
 
-opcion = -1
+opcion = None
 while opcion != 0:
-    print('Selecciona la opcion deseada:')
-    print('1. Codificar mensaje')
-    print('2. Decodificar mensaje')
-    print('0. Salir')
-    opcion = int(input())
+    while True:
+        print('Selecciona la opcion deseada:')
+        print('1. Codificar mensaje')
+        print('2. Decodificar mensaje')
+        print('0. Salir')
+        try:
+            opcion = int(input())
+            break
+        except:
+            print('Ingresa una opcion valida')
     if opcion == 1:
-        frase = ingresarFrase() # Solicitamos la frase y convertimos a una lista de numeros
+        frase = ingresarMsg() # Solicitamos la frase y convertimos a una lista de numeros
         n = conseguirN(frase) # Conseguimos el valor de nXn
-        matrizMsg = matrizMensaje(frase) # Convertimos la frase en matriz
+        matrizMsg = generarMatrizMsg(frase) # Convertimos la frase en matriz
         while True:
             print('Selecciona un opcion')
             print('1. Generar matriz codigo aleatorio')
             print('2. Ingresar una matriz codigo')
-            opcion2 = input()
+            opcion2 = int(input())
             if opcion2 == 1:
-                matrizCodigo = generar_matriz_enteros(n) # Generamos la matriz codigo
+                matrizCodigo = generarMatrizCodigo(n) # Generamos la matriz codigo
                 print('La matriz codigo generada es:')
                 print(matrizCodigo)
                 break
             elif opcion2 == 2:
-                matrizCodigo = ingresarMatrizCodigoInverso()
+                matrizCodigo = ingresarCodigoInverso()
                 print('La matriz codigo es:')
                 print(matrizCodigo)
                 break
@@ -36,4 +41,27 @@ while opcion != 0:
                 print('Elige una opcion valida')
                 continue
         matrizCodificada = codificar(matrizMsg, matrizCodigo)
+        print('El mensaje codificado es:')
         print(matrizCodificada)
+    
+    elif opcion == 2:
+        print('Cuanto mide tu matriz mensaje:')
+        print('Ingrese la medida de n1:')
+        n1 = int(input())
+        print('Ingrese la medida de n2:')
+        n2 = int(input())
+        msg = ingresarMensajeEncriptado(n1, n2)
+        print(msg)
+        mCodigo = ingresarCodigoInverso()
+        print(mCodigo)
+        codigoInverso = invertirMatriz(mCodigo)
+        msgDescifrado = descifrarMensaje(msg, codigoInverso)
+        print(msgDescifrado)
+        mMensaje = convertirMensajeDescifrado(msgDescifrado)
+        print(mMensaje)
+    
+    elif opcion == 0:
+        print('Adios!')
+
+    else:
+        print('Elige una opcion correcta')
